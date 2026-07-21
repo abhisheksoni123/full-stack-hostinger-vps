2034a94bda3611eda4ea666b914a098514e90a29
abhisheksoni123 <abhisheksoni@abhisheks-MBP-2.lan>
2026-07-22 01:21:05 +0530

    first push to production

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("API Response:", data);
+        setMessage(data.message);
+      })
+      .catch((error) => console.error("API Error:", error));
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome to chai with Abhi</h1>
+      <h2>Data {message}</h2>
+    </>
+  );
+}
+
+export default App;
