be49eb167358855029c266f123e7aed76d0e3f97
abhisheksoni123 <abhisheksoni@abhisheks-MBP-2.lan>
2026-07-28 03:29:55 +0530

    check index changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to chai with Abhi ch. soni hostinger </h1>
+      <h1>YOO Welcome to chai with Abhi ch. soni hostinger </h1>
       <h2>Data {message}</h2>
     </>
   );
