2875c6c124a04911f0fabfa10b49d9326cbf893f
abhisheksoni123 <abhisheksoni@abhisheks-MacBook-Pro-2.local>
2026-07-29 13:56:55 +0530

    update in db.js

--- server/db.js
+++ server/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection failed:");
+    console.error(error);
   }
 };
 
